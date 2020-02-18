f1d10350a18a10833076a5fb1b884a05c7312143
Carlo Santos <59646842+johnsa-bss@users.noreply.github.com>
2020-02-18 11:32:38 +0800

    Updates

---
+++ precache-manifest.ec9995f7d09c12cc32f95ae38d4cd275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2313ba9b54105693eed7364de1506e",
+    "url": "/easy-recipe/index.html"
+  },
+  {
+    "revision": "469859d6939ddcc19e4b",
+    "url": "/easy-recipe/static/css/main.f741a291.chunk.css"
+  },
+  {
+    "revision": "16ae4a8b479bddf3c6ce",
+    "url": "/easy-recipe/static/js/2.3f7a5b46.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/easy-recipe/static/js/2.3f7a5b46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "469859d6939ddcc19e4b",
+    "url": "/easy-recipe/static/js/main.a64fe28d.chunk.js"
+  },
+  {
+    "revision": "806b2d848fe4a0aa1b88",
+    "url": "/easy-recipe/static/js/runtime-main.bc88dfff.js"
+  }
+]);
